cd9424fa4f368fdf5e63838c383527f3df2f29f6
Mamta <120008345+Mamta13kunal@users.noreply.github.com>
2023-07-04 01:34:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
-
+let input=document.getElementsByTagName("input");
+let str=input[0].value;
 function palindrome(str){
-
+for(let i=0;i<str.length;i++){
+	let k=i;
+	let m=str.length-1;
+	if(str.charAt(k)==str.charAt(m)){
+		k++;
+		m--;
+	}
+	else{
+		return false;
+	}
+}
+	return true;
 }
 module.exports = palindrome
